777d92399ec46a4565a80ef2b52d6fcb1378b62c
abouttt <qw4898@naver.com>
2022-01-18 19:17:18 +0900

    Upload TowerDefence

---
+++ InfiniteStairs/TowerDefenseProject/Assets/Scripts/BlinkMuzzle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkMuzzle : MonoBehaviour
+{
+    private readonly float _blinkIntervalSec = 0.05f;
+    private Renderer _render = null;
+
+    private void Start()
+    {
+        _render = transform.GetComponent<Renderer>();
+    }
+
+    private void OnEnable()
+    {
+        if (_render != null) _render.enabled = true;
+        StartCoroutine(Blinking());
+    }
+
+    private IEnumerator Blinking()
+    {
+        while (true)
+        {
+            while (_render == null) 
+                yield return null;
+
+            _render.enabled = !_render.enabled;
+            yield return new WaitForSeconds(_blinkIntervalSec);
+        }
+    }
+}
---
+++ TowerDefenseProject/Assets/Scripts/SelfDeactiveator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class SelfDeactiveator : MonoBehaviour
+{
+    [SerializeField]
+    private string _poolName = null;
+    [SerializeField]
+    private float _returnTimeSec = 0.0f;
+
+    public void OnEnable()
+    {
+        StartCoroutine(ReturnToPool());
+    }
+
+    private IEnumerator ReturnToPool()
+    {
+        yield return new WaitForSeconds(_returnTimeSec);
+
+        ObjectPoolManager.Instance.HitSparkEftPool.Return(gameObject);
+    }
+}
